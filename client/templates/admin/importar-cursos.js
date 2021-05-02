9650187e604131ad078978f9f55251e42605c2a8
edu2969 <edtronco@gmail.com>
2021-05-02 15:58:19 -0400

    Importacion corregida
    - Correccion del Excel y de la forma de importarlo.

--- client/templates/admin/importar-cursos.js
+++ client/templates/admin/importar-cursos.js
@@ -17,19 +17,25 @@ Template.importarcursos.events({
 				for(let curso = 0; curso < wb.SheetNames.length; curso++ ) {
 					const ws = wb.Sheets[wb.SheetNames[curso]];
 					var columnas = ["numero", "rut", "nombres"];
-					let j = 7;
+					let j = 5;
 					var registros = [];
 					let etiqueta = wb.SheetNames[curso];
-					let nivel = etiqueta.split("°")[0];
-					let numero = etiqueta.split("°")[1];
+					let nivel = etiqueta.split("° ")[0];
+					let numero = etiqueta.split("° ")[1];
 					const nombreCurso = nivel + numero;
 					cursos[nombreCurso] = [];
           console.log("Procesando curso", nombreCurso);
 					while (ws["B" + j]) {
-            var rut = ws["B" + j].v;
-						var apellidos = ws["C" + j].v;
-						var nombres = ws["E" + j].v;
-            console.log("Procesando alumno", rut);
+            var rut = ws["C" + j].v;
+						var nombreCompleto = ws["D" + j].v;
+						var partes = nombreCompleto.split(" ");
+						var apellidos = partes[0] + " " + partes[1];
+						if(partes[0]=="SAN" || partes[1]=="SAN") {
+							apellidos += partes[2] + " ";
+						}
+						apellidos = apellidos.trim();
+						var nombres = nombreCompleto.substr(apellidos.length + 1).trim();
+            console.log("Procesando alumno", rut, "[" + nombres + "/" + apellidos + "]");
 						cursos[nombreCurso].push({
 							rut: rut.trim().replace(/\./g, ''),
 							nombres: nombres.trim(),
@@ -41,7 +47,6 @@ Template.importarcursos.events({
 				}
 
         console.log("Se ha procesao del excel:", doc);
-
 				Meteor.call("IngresarAlumnos", doc, function(err, resp) {
           if(!err) {
 						console.log("Todo correcto....!");
